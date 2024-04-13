7802530e5811b3c7b54cbc1e88f03b40c20d3727
hicham sayidi <hicham13sayidi@gmail.com>
2024-04-13 01:23:21 +0000

    hey

--- Desktop/React/app_todo/src/App.jsx
+++ Desktop/React/app_todo/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import React from 'react'
 import TodoList from './todoList';
+import TaskList from "./TaskList";
+
 
 function App() {
   const title = <h1>Todo List :</h1>;
   
   return (
     <>
-      {title}
+     <TaskList/>
+      {/* {title}
       
-      <TodoList />
+      <TodoList /> */}
     </>
   )
 }
